48ee424824e6b1ed531cb3617e90f8a3ec4adfbd
nazarov.yuriy.pavlovich <nazarov.yuriy.pavlovich@gmail.com>
2011-09-18 07:08:00 +0000

    Added copyright.

--- mapobject.h
+++ mapobject.h
@@ -1,3 +1,30 @@
+/****************************************************************************\
+
+    file: mapobject.h
+
+    This file is part of HumbleMaps.
+
+    Copyright (C) 2011 Ermolaev Aleksey
+    Copyright (C) 2011 Nazarov Yuriy
+    Copyright (C) 2011 Chubenko Stanislav
+
+    HumbleMaps is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 2 of the License, or
+    (at your option) any later version.
+
+    HumbleMaps is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with HumbleMaps. If not, see <http://www.gnu.org/licenses/>.
+
+
+
+\****************************************************************************/
+
 #ifndef MAPOBJECT_H
 #define MAPOBJECT_H
 
